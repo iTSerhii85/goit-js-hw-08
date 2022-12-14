dc67905309921c545346689868c479360347e668
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-14 21:10:56 +0200

    make task - 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+// ! Получил доступ к диву
+const galleryEl = document.querySelector('.gallery');
+
+// ! Создал разметку
+const markup = galleryItems.reduce
+((acc,{preview, original, description}) => acc +
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt='${description}'/>
+</a>`
+,'');
+
+// ! Добавил разметку
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a');
+
