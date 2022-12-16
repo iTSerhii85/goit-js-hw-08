0969e561f49994009caa65de8dc9eff9bdf4f42a
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-16 19:32:54 +0200

    make task - 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
 
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+setTime();
+
+    player.on('timeupdate', throttle(getTime, 1000));
+
+    function getTime () {
+        player.getCurrentTime().then(function(seconds) {
+            localStorage.setItem(LOCALSTORAGE_KEY, seconds)
+        }).catch(function(error) {
+            console.log(error);
+        });
+    };
+
+    function setTime () {
+        const startTime = localStorage.getItem(LOCALSTORAGE_KEY);
+        player.setCurrentTime(startTime).
+        catch(function(error) {
+            switch (error.name) {
+                case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                    break;
+                default:
+                // some other error occurred
+                    break;
+            };
+        });
+    };
