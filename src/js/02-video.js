b5ccad9901899880ade2d0eee987af45109edc69
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-16 20:27:51 +0200

    pritier

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,36 @@
-
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 setTime();
 
-    player.on('timeupdate', throttle(getTime, 1000));
+player.on('timeupdate', throttle(getTime, 1000));
 
-    function getTime () {
-        player.getCurrentTime().then(function(seconds) {
-            localStorage.setItem(LOCALSTORAGE_KEY, seconds)
-        }).catch(function(error) {
-            console.log(error);
-        });
-    };
+function getTime() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
-    function setTime () {
-        const startTime = localStorage.getItem(LOCALSTORAGE_KEY);
-        player.setCurrentTime(startTime).
-        catch(function(error) {
-            switch (error.name) {
-                case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                    break;
-                default:
-                // some other error occurred
-                    break;
-            };
-        });
-    };
+function setTime() {
+  const startTime = localStorage.getItem(LOCALSTORAGE_KEY);
+  player.setCurrentTime(startTime).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+}
