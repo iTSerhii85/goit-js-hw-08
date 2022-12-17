0a6b28284ca24393d998d9055d750097c605e455
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-17 11:37:08 +0200

    start tasck 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+import debounce from "lodash.debounce";
 
+const form = document.querySelector('.feedback-form');
+// const submitButton = form.querySelector('button');
+
+form.addEventListener('input', debounce(onInput, 500));
+form.addEventListener('submit', onClick);
+
+function onInput(evt){
+    console.log(evt.target.value);
+};
+
+function onClick(evt) {
+    evt.preventDefault();
+    const {elements: {email, message}} = evt.target;
+    console.log(email.value, message.value);
+}
